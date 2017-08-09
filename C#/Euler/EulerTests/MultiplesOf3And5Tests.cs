aa552d80a7f45359653e820639fd0fbe86f849e0
omxian <104002782@qq.com>
2017-08-09 10:35:39 +0800

    VS2015 C# Project/TestCase. Solve Problem 1.

---
+++ C#/Euler/EulerTests/MultiplesOf3And5Tests.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Euler;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Euler.Tests
+{
+    [TestClass()]
+    public class MultiplesOf3And5Tests
+    {
+        [TestMethod()]
+        public void FindSumNumberSolution1Test()
+        {
+            Assert.AreEqual(23,new MultiplesOf3And5().FindSumNumberSolution1(10));
+            Assert.AreEqual(233168, new MultiplesOf3And5().FindSumNumberSolution1(1000));
+        }
+    }
+}
