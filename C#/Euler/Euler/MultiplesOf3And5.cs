23f458e1ab851d5f20542626cd27081f06116d71
omxian <104002782@qq.com>
2017-08-09 11:27:22 +0800

    Problem 1 其他解法

--- C#/Euler/Euler/MultiplesOf3And5.cs
+++ C#/Euler/Euler/MultiplesOf3And5.cs
@@ -24,5 +24,17 @@ namespace Euler
             }
             return sum;
         }
+
+        //直接计算初结果 
+        public int FindSumNumberSolution2(int below)
+        {
+            int d3 = (below - 1) / 3;
+            int d5 = (below - 1) / 5;
+            //重复计算的部分
+            int d15 = (below - 1) / 15;
+
+            //等差数列求和 
+            return 3*(d3+1)*d3/2 + 5*(d5+1)*d5/2 - 15*(d15+1)*d15/2;
+        }
     }
 }
--- C#/Euler/EulerTests/MultiplesOf3And5Tests.cs
+++ C#/Euler/EulerTests/MultiplesOf3And5Tests.cs
@@ -14,8 +14,19 @@ namespace Euler.Tests
         [TestMethod()]
         public void FindSumNumberSolution1Test()
         {
-            Assert.AreEqual(23,new MultiplesOf3And5().FindSumNumberSolution1(10));
+            Assert.AreEqual(23, new MultiplesOf3And5().FindSumNumberSolution1(10));
             Assert.AreEqual(233168, new MultiplesOf3And5().FindSumNumberSolution1(1000));
+        }
+
+        [TestMethod()]
+        public void FindSumNumberSolution2Test()
+        {
+            for (int i = 1; i < 1001; i++)
+            {
+                Assert.AreEqual(
+                   new MultiplesOf3And5().FindSumNumberSolution1(i),
+                   new MultiplesOf3And5().FindSumNumberSolution2(i));
+            }
         }
     }
 }
