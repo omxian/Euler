de06513fa74d9f7780be915a8d8450844d8013ae
omxian <104002782@qq.com>
2017-08-18 16:45:25 +0800

    problem 6 Solution/Testcase

---
+++ C#/Euler/Euler/SumSquareDifference.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Euler
+{
+    public class SumSquareDifference
+    {
+        /// <summary>
+        /// 这是比较粗暴的计算方式
+        /// 在targetNumber很大的情况下会很慢
+        /// </summary>
+        /// <param name="targetNumber"></param>
+        /// <returns></returns>
+        public long Solution1(int targetNumber)
+        {
+            //加->平方
+            long sumSquare = 0;
+            //平方->加
+            long squareSum = 0;
+
+            for (int i = 1; i <= targetNumber; i++)
+            {
+                squareSum += i * i;
+                sumSquare += i;
+            }
+
+            return (long)Math.Pow(sumSquare,2) - squareSum;
+        }
+
+        /// <summary>
+        /// 使用公式直接计算
+        /// </summary>
+        public long Solution2(int targetNumber)
+        {
+            //等差数列求和 -> 平方
+            long sumSquare = targetNumber*(targetNumber+1)/2;
+            sumSquare *= sumSquare;
+            //平方和公式
+            long squareSum = ((2*targetNumber+1) * (targetNumber +1) * targetNumber)/6;
+
+            return sumSquare - squareSum;
+        }
+    }
+}
---
+++ C#/Euler/EulerTests/SumSquareDifferenceTests.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Euler;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Euler.Tests
+{
+    [TestClass()]
+    public class SumSquareDifferenceTests
+    {
+        [TestMethod()]
+        public void Solution1Test()
+        {
+            Assert.AreEqual(2640, new SumSquareDifference().Solution1(10));
+            Assert.AreEqual(25164150, new SumSquareDifference().Solution1(100));
+        }
+
+        [TestMethod()]
+        public void Solution2Test()
+        {
+            Assert.AreEqual(2640, new SumSquareDifference().Solution2(10));
+            Assert.AreEqual(25164150, new SumSquareDifference().Solution2(100));
+        }
+    }
+}
