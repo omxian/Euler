667d26948ee1e1ff77d428b1c469c2a8a3dc6e7a
omxian <104002782@qq.com>
2017-08-22 15:13:15 +0800

    Problem 8 Testcase

---
+++ C#/Euler/EulerTests/LargestProductInASeriesTests.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Euler;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Euler.Tests
+{
+    [TestClass()]
+    public class LargestProductInASeriesTests
+    {
+        [TestMethod()]
+        public void Solution1Test()
+        {
+            Assert.AreEqual(5832, new LargestProductInASeries().Solution1(4));
+            Assert.AreEqual(23514624000, new LargestProductInASeries().Solution1(13));
+        }
+    }
+}
