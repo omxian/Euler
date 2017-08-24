874661dbe89868b746663fd96c11b56216e77c8f
omxian <104002782@qq.com>
2017-08-24 08:45:58 +0800

    Problem 10 / testcase

---
+++ C#/Euler/Euler/SummationOfPrimes.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Euler
+{
+    public class SummationOfPrimes
+    {
+        public long Solution1(long number)
+        {
+            long result = 0;
+            for (long i = 2; i < number; i++)
+            {
+                if (Helper.IsPrime(i))
+                {
+                    result += i;
+                }
+            }
+            return result;
+        }
+    }
+}
---
+++ C#/Euler/EulerTests/SummationOfPrimesTests.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Euler;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Euler.Tests
+{
+    [TestClass()]
+    public class SummationOfPrimesTests
+    {
+        [TestMethod()]
+        public void Solution1Test()
+        {
+            Assert.AreEqual(17, new SummationOfPrimes().Solution1(10));
+            Assert.AreEqual(142913828922, new SummationOfPrimes().Solution1(2000000));
+        }
+    }
+}
