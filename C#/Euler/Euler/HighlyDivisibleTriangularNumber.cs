2d3aa4c2b70288fc53c68e1646a29a45d0d16c5f
omxian <104002782@qq.com>
2017-08-29 16:04:40 +0800

    problem 12

---
+++ C#/Euler/Euler/HighlyDivisibleTriangularNumber.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Euler
+{
+    public class HighlyDivisibleTriangularNumber
+    {
+        // What is the value of the first triangle number to have over five hundred divisors?
+        //n为需要查找的因子数
+        public long Solution1(int n)
+        {
+            int triangularNumber = 1;
+            int index = 1;
+            int factorNumber = 1;
+            
+            while (n > factorNumber)
+            {
+                index ++;
+                //Next Triangular Number
+                triangularNumber = index * (index + 1) / 2;
+
+                factorNumber = FindFactorNumber(triangularNumber);
+            }
+
+            return triangularNumber;
+        }
+
+        /// <summary>
+        /// 返回因子数量，优化了下这个方法就可以计算出来了
+        /// </summary>
+        private int FindFactorNumber(int n)
+        {
+            int factorNumber = 0;
+
+            for (int i = 1; i <= (int)Math.Sqrt(n); i++)
+            {
+                if (n % i == 0)
+                {
+                    factorNumber += 2;
+                }
+            }
+
+            if (Math.Sqrt(n) == (int)Math.Sqrt(n))
+            {
+                factorNumber -= 1;
+            }
+
+            return factorNumber;
+        }
+    }
+}
---
+++ C#/Euler/EulerTests/HighlyDivisibleTriangularNumberTests.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Euler;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Euler.Tests
+{
+    [TestClass()]
+    public class HighlyDivisibleTriangularNumberTests
+    {
+        [TestMethod()]
+        public void HighlyDivisibleTriangularNumber_Solution1Test()
+        {
+            Assert.AreEqual(28, new HighlyDivisibleTriangularNumber().Solution1(5));
+            Assert.AreEqual(76576500, new HighlyDivisibleTriangularNumber().Solution1(500));
+        }
+    }
+}
