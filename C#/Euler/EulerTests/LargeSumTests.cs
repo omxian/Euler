50acf4b75135c6d3b4125d7092d46cf5e3365dd8
omxian <104002782@qq.com>
2017-08-30 18:36:51 +0800

    problem 13

---
+++ C#/Euler/EulerTests/LargeSumTests.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Euler;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Euler.Tests
+{
+    [TestClass()]
+    public class LargeSumTests
+    {
+        [TestMethod()]
+        public void LargeSumTests_SolutionTest()
+        {
+            Assert.AreEqual("5537376230", new LargeSum().Solution(50,100));
+        }
+    }
+}
