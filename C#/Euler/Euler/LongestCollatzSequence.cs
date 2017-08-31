8516f1ccd70c4741c263fc71836552f113506507
omxian <104002782@qq.com>
2017-08-31 18:12:22 +0800

    Promble 14 //最后需要输出数字，而不是计数 //醉了折腾半小时- -，英语不好真是硬伤

---
+++ C#/Euler/Euler/LongestCollatzSequence.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Euler
+{
+    public class LongestCollatzSequence
+    {
+        public int Solution1(int target)
+        {
+            int maxCounter = 0;
+            int lastNumber = 0;
+
+            for (int i = 1; i < target; i++)
+            {
+                long n = i;
+                int counter = 1;
+
+                while (n != 1)
+                {
+                    counter++;
+
+                    if (n % 2 == 0)
+                    {
+                        n /= 2;
+                    }
+                    else
+                    {
+                        n = 3 * n + 1;
+                    }
+                }
+
+                if (counter > maxCounter)
+                {
+                    maxCounter = counter;
+                    lastNumber = i;
+                }
+            }
+
+            //最后需要输出数字，而不是计数
+            //醉了折腾半小时- -，英语不好真是硬伤
+            return lastNumber;
+        }
+    }
+}
---
+++ C#/Euler/EulerTests/LongestCollatzSequenceTests.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Euler;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Euler.Tests
+{
+    [TestClass()]
+    public class LongestCollatzSequenceTests
+    {
+        [TestMethod()]
+        public void Solution1Test()
+        {
+            Assert.AreEqual(837799, new LongestCollatzSequence().Solution1(1000000));
+        }
+    }
+}
