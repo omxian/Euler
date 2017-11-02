ecbd16f4fe52d287a34eb0732c17349d84fccb22
Owen <104002782@qq.com>
2017-11-02 15:29:07 +0800

    Problem15 and testcase

---
+++ C#/Euler/Euler/LatticePaths.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Euler
+{
+    public class LatticePaths
+    {
+        private int endX;
+        private int endY;
+        public long Solution1(int width,int height)
+        {
+            endX = width;
+            endY = height;
+            return FindPath(0,0);
+        }
+
+        public long Solution2(int width, int height)
+        {
+            return FindPath2(width, height);
+        }
+        
+        /// <summary>
+        /// 这个方法可以得出正确答案，但是效率极其低下
+        /// 基本思路是从头开始找，找能到终点路径的数量总和
+        /// 反面教材
+        /// </summary>
+        /// <param name="x"></param>
+        /// <param name="y"></param>
+        /// <returns></returns>
+        public long FindPath(int x,int y)
+        {
+            if(x > endX || y > endY)
+            {
+                return 0;
+            }
+            else if(x == endX && y == endY)
+            {
+                return 1;
+            }
+            else
+            {
+                if(x == endX)
+                {
+                    return FindPath(x, y + 1);
+                }
+                else if(y == endY)
+                {
+                    return FindPath(x + 1, y);
+                }
+                else
+                {
+                    return FindPath(x + 1, y) + FindPath(x, y + 1);
+                }
+            }
+        }
+        /// <summary>
+        /// 思路：是从0,0开始，右侧和下侧只有1种来源路径，中间2种来源路径，以此类推。
+        /// 见: https://mathschallenge.net/full/random_routes
+        /// </summary>
+        public long FindPath2(int width,int height)
+        {
+            long[,] arr = new long[width + 1, height + 1];
+            arr[0, 0] = 1;
+
+            for(int i=0; i<width+1;i++)
+            {
+                for(int j=0; j<height+1; j++)
+                {
+                    if(i==0 && j ==0)
+                    {
+                        continue;
+                    }
+                    if(i==0)
+                    {
+                        arr[i, j] = arr[i, j - 1];
+                    }
+                    else if(j==0)
+                    {
+                        arr[i, j] = arr[i - 1, j];
+                    }
+                    else
+                    {
+                        arr[i, j] = arr[i, j - 1] + arr[i - 1, j];
+                    }
+                }
+            }
+
+            return arr[width,height];
+        }
+    }
+}
---
+++ C#/Euler/EulerTests/LatticePathsTests.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Euler;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Euler.Tests
+{
+    [TestClass()]
+    public class LatticePathsTests
+    {
+        [TestMethod()]
+        public void Solution1Test()
+        {
+            Assert.AreEqual(2, new LatticePaths().Solution2(1, 1));
+            Assert.AreEqual(6, new LatticePaths().Solution2(2, 2));
+            Assert.AreEqual(20, new LatticePaths().Solution2(3, 3));
+            Assert.AreEqual(70, new LatticePaths().Solution2(4, 4));
+            Assert.AreEqual(252, new LatticePaths().Solution2(5, 5));
+            Assert.AreEqual(924, new LatticePaths().Solution2(6, 6));
+            Assert.AreEqual(184756, new LatticePaths().Solution2(10, 10));
+            Assert.AreEqual(155117520, new LatticePaths().Solution2(15, 15));
+            Assert.AreEqual(601080390, new LatticePaths().Solution2(16, 16));
+            Assert.AreEqual(137846528820, new LatticePaths().Solution2(20, 20));
+        }
+    }
+}
