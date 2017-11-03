74a2ebf9d6ce66afc7e45338d57dc51c54ae3881
Owen <104002782@qq.com>
2017-11-03 14:21:25 +0800

    Problem 17 & testcase

---
+++ C#/Euler/Euler/NumberLetterCounts.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Euler
+{
+    public class NumberLetterCounts
+    {
+        int[] number10To100 = new int[] {0,3,6,6,5,5,5,7,6,6};
+        int[] number1To20 = new int[] {0,3,3,5,4,4,3,5,5,4,3,6,6,8,8,7,7,9,8,8,6};
+        public int Solution(int toNumber)
+        {
+            int count = 0;
+            for(int i=1; i<= toNumber; i++)
+            {
+                count += Count(i);
+            }
+
+            return count;
+        }
+
+        public int Count(int num)
+        {
+            int count = 0;
+            if (num <= 999)
+            {
+                int hun = num / 100;
+                int remain = num % 100;
+                int ten = remain / 10;
+                int one = num % 10;
+
+                if (hun != 0)
+                {
+                    count += (7 + number1To20[hun]);
+                }
+
+                if (remain > 0 && hun != 0)
+                {
+                    //and
+                    count += 3;
+                }
+
+                if(remain <= 20)
+                {
+                    count += number1To20[remain];
+                }
+                else
+                {
+                    if (ten != 0)
+                    {
+                        count += number10To100[ten];
+                    }
+
+                    if (one != 0)
+                    {
+                        count += number1To20[one];
+                    }
+                }
+            }
+            else if (num == 1000)
+            {
+                count += 11;
+            }
+            return count;
+        }
+    }
+}
---
+++ C#/Euler/EulerTests/NumberLetterCountsTests.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Euler;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Euler.Tests
+{
+    [TestClass()]
+    public class NumberLetterCountsTests
+    {
+        [TestMethod()]
+        public void SolutionTest()
+        {
+            Assert.AreEqual(19, new NumberLetterCounts().Solution(5));
+            Assert.AreEqual(36, new NumberLetterCounts().Solution(9));
+            Assert.AreEqual(70+36, new NumberLetterCounts().Solution(19));
+            Assert.AreEqual(70 + 36 + 748, new NumberLetterCounts().Solution(99));
+            Assert.AreEqual(70 + 36 + 748 + 10, new NumberLetterCounts().Solution(100));
+            Assert.AreEqual(21113, new NumberLetterCounts().Solution(999));
+            Assert.AreEqual(21124, new NumberLetterCounts().Solution(1000));
+        }
+
+        [TestMethod()]
+        public void CountTest()
+        {
+            Assert.AreEqual(23, new NumberLetterCounts().Count(342));
+            Assert.AreEqual(20, new NumberLetterCounts().Count(115));
+        }
+    }
+}
