fdb4845032b92640e611d2caa3df2b762043309a
omxian <104002782@qq.com>
2018-07-22 05:21:40 +0800

    problem 22

---
+++ C#/Euler/Euler/NamesScores.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Euler
+{
+    public class NamesScores
+    {
+        private string[] sortedNames;
+        public long GetTotalScores()
+        {
+            ReadFile();
+            return CountNames();
+        }
+
+        private void ReadFile()
+        {
+            sortedNames = File.ReadAllText("../../../File/p022_names.txt").Split(',');
+            Array.Sort(sortedNames);
+        }
+
+        private long CountNames()
+        {
+            long sum = 0;
+            for (int i = 0; i < sortedNames.Length; i++)
+            {
+                string name = sortedNames[i];
+                sum += CountWord(name) * (Array.IndexOf(sortedNames, name) + 1);
+            }
+            return sum;
+        }
+
+        public int CountWord(string word)
+        {
+            int sum = 0;
+            for (int i = 0; i < word.Length; i++)
+            {
+                //忽略多余的"
+                if (word[i] == '\"') 
+                {
+                    continue;
+                }
+
+                sum += word[i] - 64;
+            }
+            return sum;
+        } 
+    }
+}
---
+++ C#/Euler/EulerTests/NamesScoresTests.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Euler;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Euler.Tests
+{
+    [TestClass()]
+    public class NamesScoresTests
+    {
+        [TestMethod()]
+        public void GetTotalScoresTest()
+        {
+            Assert.AreEqual(871198282, new NamesScores().GetTotalScores());
+            Assert.AreEqual(53, new NamesScores().CountWord("COLIN"));
+        }
+    }
+}
